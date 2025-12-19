2a5acf04b9650b58bce0ed812f514740cf2b93cd
nqtwxtz <nut766933@gmail.com>
2025-12-19 15:03:43 +0700

    update lab9_2

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -1,24 +1,14 @@
-#include<iostream>
-using namespace std;
+void printO(int N, int M) {
+    if (N <= 0 || M <= 0) {
+        cout << "Invalid input";
+        return;
+    }
 
-//Write the function printO() here
-
-int main(){
-	
-	printO(2,2);
-	cout << "\n";
-	
-	printO(3,5);
-	cout << "\n";	
-	
-	printO(5,3);
-	cout << "\n";	
-	
-	printO(0,3);
-	cout << "\n";	
-	
-	printO(7,-1);
-	cout << "\n";	
-	
-	return 0;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            cout << "O";
+        }
+        cout << endl;
+    }
 }
+
